1fc0d3d7b7f66449628ff19edbba16764850f44a
briankulei98@gmail.com <briankulei98@gmail.com>
2021-07-24 12:25:01 +0300

    add httpClient constractor

--- src/app/services/searchrepo.service.ts
+++ src/app/services/searchrepo.service.ts
@@ -7,7 +7,7 @@ import { HttpClient } from '@angular/common/http';
 export class SearchrepoService {
 
 
-  // constructor(private http: HttpClient) {
+  constructor(private http: HttpClient) {
 
-  // }
+  }
 }
